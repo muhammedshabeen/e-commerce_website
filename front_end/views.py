6b27b37a9faff37a89030d7aa5360f66c3ec38f5
muhammedshabeen <muhammedshabeen982@gmail.com>
2024-11-07 13:39:35 +0530

    product added in home page

--- front_end/views.py
+++ front_end/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from master.models import *
+from django.db.models import Prefetch
 
 
 def home(request):
-    return render(request,'home/base.html')
+    product_objs = Product.objects.order_by('-id').prefetch_related(Prefetch('Product_Items', queryset=ProductVarients.objects.all()))[:10]
+    print("product_objs",product_objs)
+    context = {
+        "product_objs":product_objs,
+    }
+    return render(request,'home/base.html',context)
 
 def about_us(request):
     return render(request,'home/about_us.html')
